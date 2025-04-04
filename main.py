61595d2a724ece2827dcbc16102b2514752e892f
Sean Gilday <spgilday19@gmail.com>
2025-04-04 11:41:17 -0400

    Stubbed methods

---
+++ main.py
@@ -0,0 +1,18 @@
+# main method
+def main():
+    return
+
+# scrape the webpage for parts
+def scrape():
+    return
+
+# check price of part
+def checkPrice():
+    return
+
+# send mail notification to user
+def sendMail():
+    return
+
+if __name__ == "__main__":
+    main()
